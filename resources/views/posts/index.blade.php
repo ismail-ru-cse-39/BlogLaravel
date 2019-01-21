a0b783dedd96af89c72f04bc3a6865f1b5fe226e
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-01-21 12:15:43 -0800

    we have routed the 'Edit' and 'View' button

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -45,7 +45,7 @@
 							<td>{{ substr($post->body, 0, 50)}}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
 							<td>{{date('M j, Y', strtotime($post->created_at)) }}</td>
 
-							<td><a href="#" class="btn btn-default">View</a><a href="#" class="btn btn-default">Edit</a></td>
+							<td><a href="{{ route('posts.show',$post->id) }}" class="btn btn-default btn-sm">View</a><a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
 						</tr>
 
 					@endforeach
