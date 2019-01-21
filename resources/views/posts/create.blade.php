33d6652de82e42112d6e5781eccbe128b06d7a62
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-01-20 19:45:13 -0800

    validation has done

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,7 +16,7 @@
 
 			{!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
     			{{ Form::label('title', 'Title:') }}
-    			{{ Form::text('title', null, array('class' => 'form-control', 'required' => '')) }}
+    			{{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
     			{{ Form::label('body', 'Post Body:') }}
     			{{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
@@ -32,4 +32,5 @@
 @section('scripts')
 
 	{!! Html::script('js/parsley.min.js') !!}
+
 @endsection
