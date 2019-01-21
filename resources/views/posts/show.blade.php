947869537165f483a262876cbff5ddb0edf405f7
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-01-20 23:10:01 -0800

    Edit and Delete button has been designed.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,40 @@
 
 @section('content')
 	
-	<h1>{{ $post->title }}</h1>
+	<div class="row">
+		<div class="col-md-8">
+			<h1>{{ $post->title }}</h1>
+			
+			<p class="lead">{{ $post -> body }} </p>
+		</div>
 
+		<div class="col-md-4">
+			<div class="well">
+				<dl class="dl-horizontal">
+					<dt>Create At:</dt>
+					<dd>time</dd>
+				</dl>
 
+				<dl class="dl-horizontal">
+					<dt>Create At:</dt>
+					<dd>time</dd>
+				</dl>
 
-	<p class="lead">{{ $post -> body }} </p>
+				<hr>
+
+				<div class="row">
+					<div class="col-sm-6">
+						<a href="#" class="btn btn-primary btn-block">Edit</a>
+					</div>
+					<div class="col-sm-6">
+						<a href="#" class="btn btn-danger btn-block">Delete</a>
+
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
 
 @endsection
