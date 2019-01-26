bb68e5e10a43637415a6976433f6136deeb094dc
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-01-25 20:56:37 -0800

    new view is created for slug and slug is working

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class BlogController extends Controller
 {
-    //
+    public function getSingle($slug){
+    	//fetch from the database
+    	$post = Post::where('slug', '=', $slug)->first();
+
+
+    	//return the view and pass in the post object
+    	
+    	return view('blog.slug')->withPost($post);
+    	
+    }
 }
